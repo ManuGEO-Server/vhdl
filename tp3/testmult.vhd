3201604c58aa9afe8e6bf06fdb49b0ded9f56f7f
ManuGEO-Server <84435150+ManuGEO-Server@users.noreply.github.com>
2021-11-22 18:21:26 +0100

    Update testmult.vhd

--- tp3/testmult.vhd
+++ tp3/testmult.vhd
@@ -1,3 +1,4 @@
+--Ajout des librairies pour les std-logic  
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.std_logic_unsigned.all;
@@ -13,6 +14,7 @@ port(multiplieur,multiplicande:std_logic_vector(7 downto 0);
 	fin:out std_logic);
 end component;
 
+-- Initialisation des signaux à 0 en binaire
 signal multiplieur,multiplicande:std_logic_vector(7 downto 0):="00000000";
 signal S:std_logic_vector(15 downto 0):="0000000000000000";
 signal go,rst,fin,clk:std_logic:='0';
@@ -22,7 +24,8 @@ begin
 	clk<=not clk after 5 ns;
 	rst<='1' after 2 ns;
 	go<='1' after 2 ns, '0' after 12 ns;
-	multiplieur<="00010111";
-	multiplicande<="00010011";
+-- Voici les deux valeurs à multiplier entre elle, via notre algorithme.
+	multiplieur<="00010111";	-- Valeur de 23 en décimale
+	multiplicande<="00010011";	-- Valeur de 19 en décimale
 
 end bench;
